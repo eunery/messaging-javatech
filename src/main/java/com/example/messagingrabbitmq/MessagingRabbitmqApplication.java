ab2386872af7ccde1f46b426e5855749246e0611
eunery <phisic.cons@yandex.ru>
2021-11-18 01:17:34 +0500

    done&comments

--- src/main/java/com/example/messagingrabbitmq/MessagingRabbitmqApplication.java
+++ src/main/java/com/example/messagingrabbitmq/MessagingRabbitmqApplication.java
@@ -1,9 +1,6 @@
 package com.example.messagingrabbitmq;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
@@ -11,44 +8,57 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class MessagingRabbitmqApplication {
 
-	static final String topicExchangeName = "spring-boot-exchange";
+	static final String fanoutExchangeName = "spring-boot-exchange";
+	// fanoutExchange возможность осуществления выборочной маршрутизации путем сравнения ключа маршрутизации
 
 	static final String queueName = "spring-boot";
 
 	@Bean
 	Queue queue() {
-		return new Queue(queueName, false);
+		return new Queue(queueName + UUID.randomUUID(), false, false, true );
 	}
+	// генерация уникального имени для каждой новой очереди
+	// durable - false(exchange является временным и будет удаляться, когда сервер/брокер будет перезагружен)
+	// exclusive - false(очередь разрешает подключаться только одному потребителю и удаляется если закроется канал)
+	// autoDelete - true(Exchange будет удален, когда будут удалены все связанные с ним очереди)
+	// Для каждого клиента вам понадобится эксклюзивная автоудаляемая очередь с уникальным названием
 
 	@Bean
-	TopicExchange exchange() {
-		return new TopicExchange(topicExchangeName);
+	FanoutExchange exchange() {
+		return new FanoutExchange(fanoutExchangeName);
 	}
+	// Для того чтобы сообщение рассылалось во все очереди вам понадобится FanoutExchange
 
 	@Bean
-	Binding binding(Queue queue, TopicExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+	Binding binding(Queue queue, FanoutExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange);
 	}
+	// создание связи(binding) к обменнику(exchange)
 
 	@Bean
-	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-			MessageListenerAdapter listenerAdapter) {
+	SimpleMessageListenerContainer container // Listener container для асинхронной обработки входящих сообщений
+			(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
 		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
-		container.setQueueNames(queueName);
-		container.setMessageListener(listenerAdapter);
+		container.setConnectionFactory(connectionFactory); // устанавливает соединение с rabbitmq
+		container.setQueues(queue()); // устанавливает все известные очереди, для которых нужна обработка
+		container.setMessageListener(listenerAdapter); // устанавливает целевой метод обработки сообщений
 		return container;
 	}
 
-	@Bean
+	@Bean // данный метод - обработчик сообщения в контейнере (подсоединяется выше)
 	MessageListenerAdapter listenerAdapter(Receiver receiver) {
 		return new MessageListenerAdapter(receiver, "receiveMessage");
+		// для того, чтобы Receiver работал - упаковываем его
+		// и вызываем в нем метод receiveMessage для обработки сообщений
+		// для того, чтобы
 	}
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		SpringApplication.run(MessagingRabbitmqApplication.class, args).close();
 	}
 
--- src/main/java/com/example/messagingrabbitmq/Runner.java
+++ src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -1,5 +1,6 @@
 package com.example.messagingrabbitmq;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Component;
 public class Runner implements CommandLineRunner {
 
 	private final RabbitTemplate rabbitTemplate;
+	// Spring AMQP RabbitTemplate для отправки и получения сообщений(получаем из контекста приложения) через RabbitMQ.
+	// Spring Boot автоматически создает фабрику соединения и RabbitTemplate,
 	private final Receiver receiver;
 
 	public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
@@ -19,9 +22,14 @@ public class Runner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Sending message...");
-		rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
-		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		Scanner scanner = new Scanner(System.in); // считываем все что вводится в консоли
+		while (true) {
+			String message = scanner.nextLine(); // записываем строку в переменную
+			if (message.equals("q")) break; // если в строке есть символ q, то завершаем программу
+			if (message.isBlank()) continue; // если пусто продолжаем
+			rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.fanoutExchangeName, "", message);
+			receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		}
 	}
 
 }
